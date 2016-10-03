6074d9c7c498632feb2654a7b65f8d32ac9b83ff
Jschloss309 <Jschloss309@gmail.com>
2016-10-03 02:23:57 -0400

    Setup simple app: cat years

--- Cat Years/ViewController.swift
+++ Cat Years/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var age: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    @IBAction func findAge(_ sender: AnyObject) {
+        
+        let enteredAge = Int(age.text!)
+        
+        if enteredAge != nil {
+            
+            let catYears = enteredAge! * 7
+            
+            resultLabel.text = "Your cat is \(catYears) in cat years"
+        } else {
+            resultLabel.text = "Please enter a number in the box"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
